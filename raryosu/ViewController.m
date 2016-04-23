62c169df1263a95e6f7fdafc268e7aff599f567e
nnsnodnb <oka.03_05@me.com>
2016-04-23 23:37:01 +0900

    Modify along the coding conventions

--- raryosu/ViewController.m
+++ raryosu/ViewController.m
@@ -10,39 +10,48 @@
 
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet UIImageView *raryosuImg;
+@property (weak, nonatomic) IBOutlet UIImageView *raryosuImage;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - Life Cycle
+
 - (void)viewDidLoad {
-	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-	self.raryosuImg.tag = 1;
-	self.raryosuImg.userInteractionEnabled = YES;
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+  _raryosuImage.tag = 1;
+  _raryosuImage.userInteractionEnabled = YES;
 }
 
 - (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - UITouch
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-	UITouch *touch = [[event allTouches] anyObject];
-	if (touch.view.tag == 1) {
-		[self addRaryosuImg];
-	}
+  UITouch *touch = [[event allTouches] anyObject];
+  if (touch.view.tag == 1) {
+    [self addRaryosuImage];
+  }
 }
 
-- (void)addRaryosuImg {
-	UIImage *img = [UIImage imageNamed:@"circle.png"];
-	UIImageView *iv = [[UIImageView alloc] initWithImage:img];
-	CGRect rect1 = CGRectMake(rand() % 200, rand() % 450, 129, 129);
-	iv.frame = rect1;
-	iv.tag = 1;
-	iv.userInteractionEnabled = YES;
-	[self.view addSubview:iv];
+#pragma mark - Increase raryosu image
+/**
+  *  らりょすアイコンを画面上に設置
+	*/
+
+- (void)addRaryosuImage {
+  UIImage *image = [UIImage imageNamed:@"circle.png"];
+  UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+  CGRect rect = CGRectMake(rand() % 200, rand() % 450, 129, 129);
+  imageView.frame = rect;
+  imageView.tag = 1;
+  imageView.userInteractionEnabled = YES;
+  [self.view addSubview:imageView];
 }
 
 @end
